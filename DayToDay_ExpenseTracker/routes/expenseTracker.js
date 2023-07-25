fd7df70507bfb112850933c9581a3b76d328e0ce
Sagarika Mohanty <sagarikamohanty.official@gmail.com>
2023-07-25 20:14:33 +0530

    addExpensePage/implementation

--- DayToDay_ExpenseTracker/routes/expenseTracker.js
+++ DayToDay_ExpenseTracker/routes/expenseTracker.js
@@ -1,10 +1,18 @@
 const express = require('express');
 
 const userController = require('../controller/user');
+const expenseController = require('../controller/expense');
 
 const router = express.Router();
 
 router.post('/user/signIn',userController.postUserSignin);
+
 router.post('/user/login',userController.postUserLogin);
+
+router.post('/expense/add-expense',expenseController.postAddExpense);
+
+router.get('/expense/get-expense',expenseController.getAllExpense);
+
+router.delete('/expense/delete/:expenseId',expenseController.deleteExpenseById);
 
 module.exports = router;
